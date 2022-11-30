bab57330cf737bd96796dafe7bf84ad382190827
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-30 20:46:57 +0300

    feat: add models descriptions

--- students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/models.py
+++ students/k33402/practical_works/Krivtsov_Pavel/simple_django_web_project/project_first_app/models.py
@@ -9,15 +9,21 @@ class Car(models.Model):
     model = models.CharField(max_length=20)
     color = models.CharField(max_length=30, blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.make} {self.model} {self.state_number}"
+
 
 class CarOwner(AbstractUser):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
-    cars = models.ManyToManyField(Car, through='Ownership')
+    cars = models.ManyToManyField(Car, through='Ownership', related_name="owners")
     date_of_birth = models.DateTimeField(blank=True, null=True)
     passport_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     nationality = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.second_name} {self.passport_number}"
 
 
 class DriversLicense(models.Model):
@@ -26,9 +32,15 @@ class DriversLicense(models.Model):
     type = models.CharField(max_length=10)
     date_of_issue = models.DateTimeField()
 
+    def __str__(self):
+        return self.certificate_number
+
 
 class Ownership(models.Model):
     owner_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
     car_id = models.ForeignKey(Car, on_delete=models.CASCADE, blank=True, null=True)
     start_date = models.DateTimeField()
     finish_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Owner: {self.owner_id}, Car: {self.car_id}"
